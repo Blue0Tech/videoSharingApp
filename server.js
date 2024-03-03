607e59b2c2918c4f6382449802dd64dce8840491
Blue0Tech <pruthvishrikaanth@gmail.com>
2024-03-03 09:19:12 +0000

    Added backend for email invites

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const server = require("http").Server(app);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.json());
 
 const { v4: uuidv4 } = require("uuid");
 
@@ -19,12 +20,44 @@ const peerServer = ExpressPeerServer(server,{
 
 app.use('/peerjs',peerServer);
 
+let nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    port: 465,
+    host: 'smtp.gmail.com',
+    auth: {
+        user: 'shrikaanthpruthvispareemail@gmail.com',
+        pass: process.env.APP_PASSWORD
+    },
+    secure: true
+});
+
 app.get('/',(req,res)=>{
     res.redirect(`/${uuidv4()}`);
 })
 
 app.get("/:room", (req, res) => {
     res.render("index", { roomId: req.params.room });
+});
+
+app.post('/send-mail',(req,res)=>{
+    const to = req.body.to;
+    const url = req.body.url;
+    const mailData = {
+        from: 'shrikaanthpruthvispareemail@gmail.com',
+        to: to,
+        subject: 'You have been invited to video chat!',
+        html: `<p>Hey there,</p><p>Come join this video chat! ${url}</p>`,
+    };
+    transporter.sendMail(mailData,(error,info)=>{
+        if(error) {
+            return console.log(error);
+        };
+        res.status(200).send({
+            message: "Invitation sent!",
+            message_id: info.messageId
+        });
+    });
 });
 
 io.on('connection',(socket)=>{
